3a27a739ee30c91d94a5cdc2296196c68bb10e0e
michelco86 <36239903+michelco86@users.noreply.github.com>
2018-04-02 22:07:02 +0200

    Update main.ino

--- 2018/main.ino
+++ 2018/main.ino
@@ -5,9 +5,9 @@ float alpha;
 float k = 1.1;
 float d;
 const float Pi = 3.141593;
-float L = 1; //car length
-float vmax = 1; //car max velocity
-float deltamax = 1; //max steering angle
+float L = 0.3; //car length
+float vmax = 5; //car max velocity
+float deltamax = 30; //max steering angle [deg]
 float kappa;
 float u[] = {0,90};
 
@@ -25,8 +25,12 @@ int dir_pin = 2;  //direction control for motor outputs 1 and 2
 int dc;
 
 /*sensor defines*/
-int sensorPin = A0;    // select the input pin for the potentiometer
-int sensorValue = 0;  // variable to store the value coming from the sensor
+const int nrOfFrontSensors = 1; //nr of sensors to use in the controller ADJUST AS FOR HOW MANY SENSORS CURRENTLY CONNECTED
+int sensorPin[] = {A0}; //array of input pins ADJUST FOR CURRENTLY CONNECTED SENSORS
+int rawData[nrOfFrontSensors]; //array to store value coming from sensor
+float sensorAngle[] = {45.0,22.5,0.0,22.5,45.0}; //MAKE SURE THE CONNECTED PIN INDEX MATCHES SENSOR ANGLES DEFINED HERE
+float sensorValue[nrOfFrontSensors];  //array to store shifted and inverted value for sensor
+float offsetValue[nrOfFrontSensors]; //array to store offset value for each sensor
 
 /*filter defines*/
 float smoothedVal1;
@@ -49,16 +53,22 @@ void setup() {
 
 void controller(float u[], float d, float alpha) {
 
+  //convert to rad for sin(x) and atan(y)
+  alpha = (alpha * Pi)/180.0;
   
-  /* pure pursuit controller%% */
-
   /* compute steering control input */
   kappa = 2.0 * sin(alpha) / d;
   u[1] = (float) -atan(kappa);
-  if (-atan(kappa) > deltamax) {
+  
+  //convert back to degrees
+  u[1] = (u[1] * 180.0)/Pi;
+  
+  if (u[1] > deltamax) {
     u[1] = deltamax;
   }
 
+
+  
   /* motor control signal (very simplistic function of steering angle) */
   u[0] = tanh(Pi - k * Pi * fabs(u[1]));
 
@@ -99,35 +109,56 @@ void motorControl(int dCycle, int direction)
     digitalWrite(dir_pin, HIGH);
   }
 
-  //send duty cycle !!!!OBS 0.5!!!!!
+  //send duty cycle !!multiplied by 0.3 because its so fast!!
   analogWrite(pwm_pin, dCycle*0.3);  
 }
 
 
-void getTargetPoint() {
+void getTargetPoint(float& dist, float& angle) {
+  int maxSensorDistance = 50;
   //read all sensor values
-  int rawData1 = analogRead(sensorPin);
+  for (int i = 0; i < nrOfFrontSensors; pos += 1) { 
+    rawData[i] = analogRead(sensorPin[i]);
+    //cap data at maxSensorDistance cm MIGHT NEED TO TWEAK
+    if(rawData[i] > maxSensorDistance) {
+      rawData[i] = maxSensorDistance;
+    }
+  }
 
-  //smooth data
-  smoothedVal1 =  smooth(rawData1, 0.9, smoothedVal1); 
+  //smooth data, try first without filtering the data
+  //smoothedVal1 =  smooth(rawData1, 0.9, smoothedVal1); 
 
   //invert and shift sensor data
-  float offset1 = 0;
-  float sensorValue1 = 1000*(1.0/smoothedVal1) + offset1;
+  for (int i = 0; i < nrOfFrontSensors; pos += 1) { 
+  sensorValue[i] = 1000*(1.0/rawData[i]) + offsetValue[i];  
+  }
 
-  Serial.print(sensorValue1);
-  Serial.println();
+  //Serial.print(sensorValue[0]);
+  //Serial.println();
 
-  //pick target point
-  /*check all sensor values, pick the value which has largest distance and send that distance and co
-   * corresponding angle to control algorithm
-   */
+  //pick target point (if sensor with angle 0 has max distance, go straight else check for which sensor has max distance)
+  if (sensorValue[2] == maxSensorDistance) {
+    dist = maxSensorDistance;
+    angle = sensorAngle[2];
+  } else { 
+    int maxIndex = 0;
+    int maxVal = sensorValue[maxIndex];
+    for (int i = 0; i < nrOfFrontSensors; pos += 1) { 
+      if (maxVal<sensorValue[i]){
+        maxVal = sensorValue[i];
+        maxIndex = i;
+      }
+    }
+  dist = maxVal;
+  angle = sensorAngle[maxIndex];   
+  }
+
 }
 
+//smoothing filter
 int smooth(int data, float filterVal, float smoothedVal){
 
-
- if (filterVal > 1){      // check to make sure param's are within range
+  if (filterVal > 1){      // check to make sure param's are within range
    filterVal = .99;
  }
  else if (filterVal <= 0){
@@ -139,19 +170,20 @@ int smooth(int data, float filterVal, float smoothedVal){
  return (int)smoothedVal;
 }
 
+//use this to test each sensor, check serial monitor for sensor value
 void sensorTest() {
   // read the value from the sensor:
-  int sensorValue = analogRead(sensorPin);
-  float value = 1000*(1.0/sensorValue);
+  int sensorIn = analogRead(sensorPin[0]);
+  float value = 1000*(1.0/sensorIn);
   Serial.print(value);
   Serial.println();
 }
 
 void loop() {
-  getTargetPoint();
-  /*controller(u,0.3,0.1);*/
-  /*motorControl(&u[0],1);*/
-  /*currAngle = servoControl(&u[1],currAngle);*/
+  getTargetPoint(d,alpha);
+  controller(u,d,alpha);
+  motorControl(&u[0],1);
+  currAngle = servoControl(&u[1],currAngle);
   /*sensorTest();*/
 
 }
