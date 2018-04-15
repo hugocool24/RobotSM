b5e9934190766207b055121340ea3f371db8bad0
michelco86 <36239903+michelco86@users.noreply.github.com>
2018-04-15 17:15:28 +0200

    Update main.ino

--- 2018/main.ino
+++ 2018/main.ino
@@ -5,7 +5,7 @@ float alpha;
 float k = 1.1;
 float d;
 const float Pi = 3.141593;
-float L = 0.3; //car length
+float L = 0.2; //car length
 float vmax = 5; //car max velocity
 float deltamax = 30; //max steering angle [deg]
 float kappa;
@@ -23,17 +23,17 @@ int pos;
 
 /*motor defines*/
 int pwm_pin = 3;  //PWM control for motor outputs 1 and 2 
-int dir_pin = 2;  //direction control for motor outputs 1 and 2 
+int dir_pin = 12;  //direction control for motor outputs 1 and 2 
 int dc;
 
 /*sensor defines*/
 const int nrOfFrontSensors = 3; //nr of sensors to use in the controller ADJUST AS FOR HOW MANY SENSORS CURRENTLY CONNECTED
 int sensorPin[] = {2,3,4}; //array of input pins ADJUST FOR CURRENTLY CONNECTED SENSORS
-int rawData[nrOfFrontSensors]; //array to store value coming from sensor
-int sensorAngle[] = {-45,0,45}; //MAKE SURE THE CONNECTED PIN INDEX MATCHES SENSOR ANGLES DEFINED HERE. Angles must be Integers
-const int midSensorIndex = 1;
-int sensorValue[nrOfFrontSensors] = {0,0,0};  //array to store shifted and inverted value for sensor
-int offsetValue[nrOfFrontSensors] = {0,0,0}; //array to store offset value for each sensor
+float rawData[nrOfFrontSensors]; //array to store value coming from sensor
+int sensorAngle[] = {0,45,-45}; //MAKE SURE THE CONNECTED PIN INDEX MATCHES SENSOR ANGLES DEFINED HERE. Angles must be Integers
+const int midSensorIndex = 0;
+float sensorValue[nrOfFrontSensors] = {0,0,0};  //array to store shifted and inverted value for sensor
+float offsetValue[nrOfFrontSensors] = {0,0,0}; //array to store offset value for each sensor
 
 /*filter defines*/
 float smoothedVal1;
@@ -43,7 +43,7 @@ float smoothedVal1;
 void setup() {
   
   // put your setup code here, to run once:
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object   
+  myservo.attach(6);  // attaches the servo on pin 9 to the servo object   
   Serial.begin(9600);
   myservo.write(90);
   
@@ -53,22 +53,29 @@ void setup() {
   pinMode(2, INPUT);
   pinMode(3, INPUT);
   pinMode(4, INPUT);
+  digitalWrite(dir_pin, HIGH);
+  digitalWrite(9, LOW); //unlock brake
   analogWrite(pwm_pin, 0);
 
 }
 
-void controller(float u[], float d, float alpha) {
+void controller(float u[], float d, float alpha, float L) {
+  
 
-  //convert to rad for sin(x) and atan(y)
+  Serial.println(alpha);
+
+  
+  //convert to rad for sin(x) and atan(y) and change sign
+  
   alpha = (alpha * Pi)/180.0;
   
   /* compute steering control input */
-  kappa = 2.0 * sin(alpha) / d;
-  u[1] = (float) -atan(kappa);
-  
+  kappa = 2.0 * sin(alpha) / (d/100.0);
+  u[1] = -atan(kappa*L);
+
   //convert back to degrees
   u[1] = (u[1] * 180.0)/Pi;
-
+  
   if (u[1] > deltamax) {
     u[1] = deltamax;
   }
@@ -77,8 +84,11 @@ void controller(float u[], float d, float alpha) {
     u[1] = -deltamax;
   }
 
-  u[1] = map(u[1],-deltamax,deltamax,0,180);
   
+  u[1] = map(u[1],-deltamax,deltamax,180-35,35);
+
+  Serial.println(u[1]);
+  //Serial.println(u[1]);
   /* motor control signal (very simplistic function of steering angle) */
   
   float angle = u[1]*(Pi/180);
@@ -88,11 +98,11 @@ void controller(float u[], float d, float alpha) {
   /*u[0] = u[0]/vmax;
 
   /* end of pure pursuit controller */
- 
+  
 }
 
 int servoControl(float u[], int currAngle) {
-    
+   
     reqAngle = (int) round(u[1]); //convert req. angle to integer after rounding
     if(reqAngle > currAngle) 
     {
@@ -108,6 +118,7 @@ int servoControl(float u[], int currAngle) {
     } else {
       myservo.write(reqAngle);
     }
+
     
     currAngle = reqAngle;
     return currAngle;
@@ -126,7 +137,7 @@ void motorControl(float u[], int direction)
 
   //construct duty cycle, lowest PWM to get the motor rolling is approx. 150. Max duty cycle is 255. Currently restricted to 200
   int dCycle = (int) round(u[0]*255.0);
-  dCycle = map(dCycle,0,255,150,200);
+  dCycle = map(dCycle,0,255,120,120);
   
   //send duty cycle
   analogWrite(pwm_pin, dCycle);  
@@ -134,23 +145,33 @@ void motorControl(float u[], int direction)
 
 
 void getTargetPoint(float& d, float& alpha) {
-  int maxSensorDistance = 40;
+  int maxSensorDistance = 80;
+  int minSensorDistance = 10;
   
   //read all sensor values
   for (int i = 0; i < nrOfFrontSensors; i += 1) { 
-    rawData[i] = analogRead(sensorPin[i]);
+    rawData[i] = analogRead(sensorPin[i])/200.0;
    }
  
   //smooth data, try first without filtering the data
   //smoothedVal1 =  smooth(rawData1, 0.9, smoothedVal1); 
 
-  //invert and shift sensor data
   for (int i = 0; i < nrOfFrontSensors; i += 1) { 
-    sensorValue[i] = 1023.0*(1.0/rawData[i]) + offsetValue[i];
+    if (rawData[i]>1) {
+      sensorValue[i] = (3.5-rawData[i])*13;
+    } else {
+      sensorValue[i] = (1.3-rawData[i])/0.01;
+    }
+
     if(sensorValue[i] > maxSensorDistance) {
       sensorValue[i] = maxSensorDistance;  
     }
-   }
+       
+    if(sensorValue[i] < minSensorDistance) {
+      sensorValue[i] = minSensorDistance;  
+    }
+  }
+
   //pick target point (if sensor with angle 0 has max distance, go straight else check for which sensor has max distance)
   //here we will maybe need to do something with the side sensors to try and stabilize the car trajectory to the center of the track
   //when we have no obstacles.
@@ -171,7 +192,6 @@ void getTargetPoint(float& d, float& alpha) {
   d = maxVal;
   alpha = sensorAngle[maxIndex];   
   }
-
 }
 
 //smoothing filter
@@ -192,33 +212,63 @@ int smooth(int data, float filterVal, float smoothedVal){
 //use this to test each sensor, check serial monitor for sensor value
 void sensorTest() {
   // read the value from the sensor:
+  Serial.println("center");
+  float sensorIn1 = analogRead(A2);
+  //float value = 1023.0*(1.0/sensorIn);
+
+  float value1;
+  sensorIn1 = sensorIn1/200.0;
+  if (sensorIn1>1){
+    value1 = (3.5-sensorIn1)*13;
+  }
+  else {
+   value1 = (1.3-sensorIn1)/0.01;
+  }
+  
+  Serial.print(value1);
+  Serial.println();
+
+  Serial.println("right");
+  float sensorIn2 = analogRead(A3);
+  //float value2 = 1023.0*(1.0/sensorIn2);
+  float value2;
+  sensorIn2 = sensorIn2/200.0;
+  if (sensorIn2>1){
+    value2 = (3.5-sensorIn2)*13;
+  }
+  else {
+   value2 = (1.3-sensorIn2)/0.01;
+  }
+  Serial.print(value2);
+  Serial.println();
+
   Serial.println("left");
-  int sensorIn = analogRead(A2);
-  float value = 1023.0*(1.0/sensorIn);
-  Serial.print(sensorIn);
-  Serial.println();
+  float sensorIn3 = analogRead(A4);
+  //float value3 = 1023.0*(1.0/sensorIn3);
 
-  Serial.println("centre");
-  int sensorIn2 = analogRead(A3);
-  float value2 = 1023.0*(1.0/sensorIn2);
-  Serial.print(sensorIn2);
-  Serial.println();
-
-  Serial.println("rigth");
-  int sensorIn3 = analogRead(A4);
-  float value3 = 1023.0*(1.0/sensorIn3);
-  Serial.print(sensorIn3);
+  float value3;
+  sensorIn3 = sensorIn3/200.0;
+  if (sensorIn3>1){
+    value3 = (3.5-sensorIn3)*13;
+  }
+  else {
+   value3 = (1.3-sensorIn3)/0.01;
+  }
+  
+  Serial.print(value3);
   Serial.println();
 
 
 }
 
+
+
 void loop() {
   getTargetPoint(d,alpha);
-  controller(u,d,alpha);
-  //Serial.println(u[0]);
-  //motorControl(u,1);
-  //currAngle = servoControl(u,currAngle);
+  controller(u,d,alpha,L);
+  Serial.println(u[1]);
+  motorControl(u,2);
+  currAngle = servoControl(u,currAngle);
   delay(100); //this is to be tweaked
   //sensorTest();
 }
